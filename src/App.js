ca86c29ad3d08a48828e6badd88b9c26078664f8
Rakesh Ranjan <riitdel@gmail.com>
2022-06-15 09:04:18 +0530

    Navbar updated

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function App() {
   
   return (
     <div>
-     
+     <NavBar></NavBar>
     <div className="App">
       <Routes>
         <Route path="/login" element={<SignInSide />}> </Route>
